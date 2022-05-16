32bc66a2792b200c4c1df374285c3417c1526a4b
Yusuf Akin <kutluhanakin@gmail.com>
2022-05-16 13:43:05 -0400

    added double down

--- src/BlackJack/playBlackJack.java
+++ src/BlackJack/playBlackJack.java
@@ -35,92 +35,137 @@ public class playBlackJack {
                 int dealerTotal = dealerCard1Value + dealerCard2Value;
                 System.out.println("Your cards are " + card1 + " of " + CardDealer.getSuit() + " and " + card2 + " of " + CardDealer.getSuit() + " with a total of: " + playerTotal);
                 System.out.println("Dealer cards are " + dealerCard1 + " of " + CardDealer.getSuit() + " and [Unrevealed]" + " with a total of: " + dealerCard1Value);
-                //CHECKS THE TOTAL OF PLAYER'S CARD
-                if (playerTotal < 21) {
-                    System.out.println("Would you like to (H)it or (S)tand?");
-                    scanner.nextLine();
-                    String playerChoice = scanner.nextLine();
-                    if (playerChoice.equalsIgnoreCase("h")) {
-                        String card3 = card.getCard();
-                        int card3Value = card.getCardValue();
-                        //STATEMENT FOR THE ACE VALUE
-                        if ((playerTotal + 11) > 21 && (card1Value == 11 || card2Value == 11)) playerTotal -= 10;
-                        playerTotal += card3Value;
-                        System.out.println("Your new card is a " + card3 + " of " + CardDealer.getSuit() + " with a value of " + card3Value);
-                        System.out.println("Now your total value is " + playerTotal);
-                        if (playerTotal < 21) {
-                            System.out.println("Would you like to (H)it or (S)tand?");
-                            String playerChoice2 = scanner.nextLine();
-                            if (playerChoice2.equalsIgnoreCase("h")) {
-                                String card4 = card.getCard();
-                                int card4Value = card.getCardValue();
-                                //STATEMENT FOR THE ACE
-                                if ((playerTotal + 11) > 21 && card3Value == 11) playerTotal -= 10;
-                                System.out.println("Your new card is a " + card4 + " of " + CardDealer.getSuit() + " with a value of " + card4Value);
-                                playerTotal += card4Value;
-                                System.out.println("Now your total value is " + playerTotal);
-                                if (playerTotal < 21) {
-                                    System.out.println("Would you like to (H)it or (S)tand?");
-                                    String playerChoice3 = scanner.nextLine();
-                                    if (playerChoice3.equalsIgnoreCase("h")) {
-                                        String card5 = card.getCard();
-                                        int card5Value = card.getCardValue();
-                                        //STATEMENT FOR THE ACE
-                                        if ((playerTotal + 11) > 21 && card4Value == 11) playerTotal -= 10;
-                                        System.out.println("Your new card is a " + card5 + " of " + CardDealer.getSuit() + " with a value of " + card5Value);
-                                        playerTotal += card5Value;
-                                        System.out.println("Your total value is: " + playerTotal);
+                //ASKS THE USER IF THEY WANT TO DOUBLE DOWN
+                System.out.println("Do you want to double down? Enter (Y) for yes, (N) for no");
+                scanner.nextLine();
+                String doubleDownChoice = scanner.nextLine();
+                //PROMPTS THE USER FOR DOUBLE DOWN
+                if(doubleDownChoice.equals("y")){
+                    System.out.println("How much you want to bet?");
+                    int ddbet = scanner.nextInt();
+                    betInput += ddbet;
+                    String ddcard1 = card.getCard();
+                    int ddcardValue = card.getCardValue();
+                    System.out.println("Your new card is a " + ddcard1 + " of " + CardDealer.getSuit() + " with a value of " + ddcardValue);
+                    playerTotal += ddcardValue;
 
-                                        //CHECKS THE ACE IF IT IS BUSTING THE PLAYERS HAND
-                                        if ((playerTotal + 11) > 21 && card5Value == 11) {
-                                            playerTotal -= 10;
+                    if (playerTotal > 21 || dealerTotal == 21) {
+                        System.out.println("Sorry you bust!");
+                        playerMoney -= betInput;
+                        continue;
+                    }
+                    System.out.println("Dealer flips over " + dealerCard2 + " of " + CardDealer.getSuit() + " with a value of " + dealerCard2Value);
+                    System.out.println("Dealer total is " + dealerTotal);
+                    while (dealerTotal < 17) {
+                        String newDealerCard = card.getCard();
+                        int newDealerCardValue = card.getCardValue();
+                        System.out.println("Dealer flips over a " + newDealerCard + " of " + CardDealer.getSuit() + " with a value of " + newDealerCardValue);
+                        dealerTotal += newDealerCardValue;
+                        System.out.println("Dealer total is now: " + dealerTotal);
+                    }
+
+                    // EVALUATES THE HAND OF THE PLAYER AND THE DEALER
+
+                    if (playerTotal > dealerTotal || dealerTotal > 21) {
+                        System.out.println("Congrats! You have won! You have " + playerTotal + " and the Dealer has " + dealerTotal);
+                        playerMoney += betInput * 1.5;
+                        System.out.println("Your balance is: $" + playerMoney);
+                    } else if (dealerTotal > playerTotal) {
+                        System.out.println("Sorry! You have lost. You have " + playerTotal + "and the Dealer has " + dealerTotal);
+                        playerMoney -= betInput;
+                        System.out.println("Your balance is $" + playerMoney);
+                        //EVALUATES THE BLACKJACK
+                    } else if (playerTotal == 21) {
+                        System.out.println("BlackJack! Your balance is $" + playerMoney);
+                    } else
+                        System.out.println("It's a tie!");
+                } else if(doubleDownChoice.equals("n")){
+                    if (playerTotal < 21) {
+                        System.out.println("Would you like to (H)it or (S)tand?");
+                        String playerChoice = scanner.nextLine();
+                        if (playerChoice.equalsIgnoreCase("h")) {
+                            String card3 = card.getCard();
+                            int card3Value = card.getCardValue();
+                            //STATEMENT FOR THE ACE VALUE
+                            if ((playerTotal + 11) > 21 && (card1Value == 11 || card2Value == 11)) playerTotal -= 10;
+                            playerTotal += card3Value;
+                            System.out.println("Your new card is a " + card3 + " of " + CardDealer.getSuit() + " with a value of " + card3Value);
+                            System.out.println("Now your total value is " + playerTotal);
+                            if (playerTotal < 21) {
+                                System.out.println("Would you like to (H)it or (S)tand?");
+                                String playerChoice2 = scanner.nextLine();
+                                if (playerChoice2.equalsIgnoreCase("h")) {
+                                    String card4 = card.getCard();
+                                    int card4Value = card.getCardValue();
+                                    //STATEMENT FOR THE ACE
+                                    if ((playerTotal + 11) > 21 && card3Value == 11) playerTotal -= 10;
+                                    System.out.println("Your new card is a " + card4 + " of " + CardDealer.getSuit() + " with a value of " + card4Value);
+                                    playerTotal += card4Value;
+                                    System.out.println("Now your total value is " + playerTotal);
+                                    if (playerTotal < 21) {
+                                        System.out.println("Would you like to (H)it or (S)tand?");
+                                        String playerChoice3 = scanner.nextLine();
+                                        if (playerChoice3.equalsIgnoreCase("h")) {
+                                            String card5 = card.getCard();
+                                            int card5Value = card.getCardValue();
+                                            //STATEMENT FOR THE ACE
+                                            if ((playerTotal + 11) > 21 && card4Value == 11) playerTotal -= 10;
+                                            System.out.println("Your new card is a " + card5 + " of " + CardDealer.getSuit() + " with a value of " + card5Value);
+                                            playerTotal += card5Value;
+                                            System.out.println("Your total value is: " + playerTotal);
+
+                                            //CHECKS THE ACE IF IT IS BUSTING THE PLAYERS HAND
+                                            if ((playerTotal + 11) > 21 && card5Value == 11) {
+                                                playerTotal -= 10;
+                                            }
                                         }
                                     }
                                 }
                             }
                         }
                     }
+
+                    if (playerTotal > 21 || dealerTotal == 21) {
+                        System.out.println("Sorry you bust!");
+                        playerMoney -= betInput;
+                        continue;
+                    }
+                    System.out.println("Dealer flips over " + dealerCard2 + " of " + CardDealer.getSuit() + " with a value of " + dealerCard2Value);
+                    System.out.println("Dealer total is " + dealerTotal);
+
+                    while (dealerTotal < 17) {
+                        String newDealerCard = card.getCard();
+                        int newDealerCardValue = card.getCardValue();
+                        System.out.println("Dealer flips over a " + newDealerCard + " of " + CardDealer.getSuit() + " with a value of " + newDealerCardValue);
+                        dealerTotal += newDealerCardValue;
+                        System.out.println("Dealer total is now: " + dealerTotal);
+                    }
+
+                    // EVALUATES THE HAND OF THE PLAYER AND THE DEALER
+
+                    if (playerTotal > dealerTotal || dealerTotal > 21) {
+                        System.out.println("Congrats! You have won! You have " + playerTotal + " and the Dealer has " + dealerTotal);
+                        playerMoney += betInput * 1.5;
+                        System.out.println("Your balance is: $" + playerMoney);
+                    } else if (dealerTotal > playerTotal) {
+                        System.out.println("Sorry! You have lost. You have " + playerTotal + "and the Dealer has " + dealerTotal);
+                        playerMoney -= betInput;
+                        System.out.println("Your balance is $" + playerMoney);
+                        //EVALUATES THE BLACKJACK
+                    } else if (playerTotal == 21) {
+                        System.out.println("BlackJack! Your balance is $" + playerMoney);
+                    } else
+                        System.out.println("It's a tie!");
+
+                    System.out.println("Do you want to play again? Enter (Y)");
+                    customerInput = scanner.nextLine();
+
                 }
-
-                if (playerTotal > 21 || dealerTotal == 21) {
-                    System.out.println("Sorry you bust!");
-                    playerMoney -= betInput;
-                    continue;
-                }
-                System.out.println("Dealer flips over " + dealerCard2 + " of " + CardDealer.getSuit() + " with a value of " + dealerCard2Value);
-                System.out.println("Dealer total is " + dealerTotal);
-
-                while (dealerTotal < 17) {
-                    String newDealerCard = card.getCard();
-                    int newDealerCardValue = card.getCardValue();
-                    System.out.println("Dealer flips over a " + newDealerCard + " of " + CardDealer.getSuit() + " with a value of " + newDealerCardValue);
-                    dealerTotal += newDealerCardValue;
-                    System.out.println("Dealer total is now: " + dealerTotal);
-                }
-
-                // EVALUATES THE HAND OF THE PLAYER AND THE DEALER
-
-                if (playerTotal > dealerTotal || dealerTotal > 21) {
-                    System.out.println("Congrats! You have won! You have " + playerTotal + "and the Dealer has " + dealerTotal);
-                    playerMoney += betInput * 1.5;
-                    System.out.println("Your balance is: $" + playerMoney);
-                } else if (dealerTotal > playerTotal) {
-                    System.out.println("Sorry! You have lost. You have " + playerTotal + "and the Dealer has " + dealerTotal);
-                    playerMoney -= betInput;
-                    System.out.println("Your balance is $" + playerMoney);
-                    //EVALUATES THE BLACKJACK
-                } else if (playerTotal == 21) {
-                    System.out.println("BlackJack! Your balance is $" + playerMoney);
-                } else
-                    System.out.println("It's a tie!");
-
-                System.out.println("Do you want to play again? Enter (Y)");
-                customerInput = scanner.nextLine();
 
             }
 
         }
-
     }
-}
+
+                }
 
